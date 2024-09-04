3938c1aeb3207f5271ce7a25e969d44fe411fff5
Serhii Sheiko <128324762+serhii-sheiko@users.noreply.github.com>
2024-09-04 22:15:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ if __name__ == "__main__":
 
         serve(app, host="0.0.0.0", port=PORT)
     else:
-        bot.infinity_pooling()
+        bot.infinity_polling()
         # app.run(debug=True)
